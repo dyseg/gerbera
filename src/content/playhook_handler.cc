f22f9fc8e8aa2537cb37721509cc9c537f257b1f
dyseg <bttf87@gmail.com>
2021-04-24 16:50:57 +0000

    move to first in last played list if already exists

--- src/content/playhook_handler.cc
+++ src/content/playhook_handler.cc
@@ -11,15 +11,21 @@ void PlayHookHandler::operator()()
 {
     log_info("start");
     content->triggerPlayHook(obj);
-    if (!std::count(content->lastOpened.begin(), content->lastOpened.end(), obj->getParentID())) {
-        log_info("Adding parent of '{}' to the last played list", obj->getTitle());
-        content->lastOpened.insert(content->lastOpened.begin(), obj->getParentID());
-        constexpr int limit = 5;
-        if(content->lastOpened.size() > limit) {
-            log_info("There are more than {} elements in the last played list. Removing oldest one", limit);
-            content->lastOpened.pop_back();
-        }
-    } else
+
+    auto itr = std::find(content->lastOpened.begin(), content->lastOpened.end(), obj->getParentID());
+
+    if (itr != content->lastOpened.cend()) { //element already in vector
         log_info("Parent of '{}' is already present in the last played list", obj->getTitle());
+        content->lastOpened.erase(itr);
+    } else
+        log_info("Adding parent of '{}' as first element to the last played list", obj->getTitle());
+    content->lastOpened.insert(content->lastOpened.begin(), obj->getParentID());
+
+    constexpr int limit = 5;
+    if (content->lastOpened.size() > limit) {
+        log_info("There are more than {} elements in the last played list. Removing oldest one", limit);
+        content->lastOpened.pop_back();
+    }
+
     log_info("end");
 }
